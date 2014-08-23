4017d96015294928964ffc6b05b1298e9ff56b69
sj33 <sj33@drexel.edu>
2014-08-22 20:47:28 -0400

    Create fp.h

---
+++ fp.h
@@ -0,0 +1,36 @@
+Enter file contents here
+#ifndef fp_H_
+#define fp_H_
+
+#include <cv.h>
+#include "highgui.h"
+#include "math.h"
+#include "stdlib.h"
+#include "stdio.h"
+
+//Stucture to hold character template images and data
+typedef struct charInfo charInfo;
+struct charInfo
+{
+	IplImage * charImage;
+	double contourArea;
+	double arcLength;
+	char* charText;
+}; 
+
+
+//Stucture to hold character template images and data
+typedef struct plateInfo plateInfo;
+struct plateInfo
+{
+	IplImage * charImage;
+	double contourArea;
+	double arcLength;
+}; 
+
+plateInfo* processPlateChars( IplImage * orig_img );
+charInfo* processTemplateChars( IplImage * template_img, int * numContours );
+char** compareChar( charInfo * char_info, plateInfo * plate_info, int * numContours );
+
+#endif /* fp_H_ */
+

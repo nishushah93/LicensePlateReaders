ba75947ccc8591a91ff17182ff883664247b3978
Nisarg Shah <nys26@cs.drexel.edu>
2014-08-23 18:36:36 -0400

    Fixing all the previous errors

--- lpstate.h
+++ lpstate.h
@@ -1,6 +1,8 @@
 #ifndef lpstate_c
 #define lpstate_c
 
+#include "cv.h"
+#include "highgui.h"
 
 double colorDistance(CvScalar c1, CvScalar c2);
 int findClosest(CvScalar t, CvScalar* scolors, int numColors);

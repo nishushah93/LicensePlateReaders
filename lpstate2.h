b8bfb9257295b64dea822615bfc8c4cf3e1e97b9
sj33 <sj33@drexel.edu>
2014-08-25 14:35:39 -0400

    Create lpstate2.h

---
+++ lpstate2.h
@@ -0,0 +1,15 @@
+
+#ifndef lpstate_c
+#define lpstate_c
+
+#include "cv.h"
+#include "highgui.h"
+
+double colorDistance(CvScalar c1, CvScalar c2);
+int findClosest(CvScalar t, CvScalar* scolors, int numColors);
+char* findState(char** filenames);
+
+
+#endif
+
+

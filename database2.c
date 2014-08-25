4c7a28d5d68d1105c2fc9ec200569b30b4da4d36
sj33 <sj33@drexel.edu>
2014-08-25 14:30:59 -0400

    Update database2.c

--- database2.c
+++ database2.c
@@ -1,2 +1,61 @@
-Enter file contents here
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "database.h"
+
+Owner newOwner(char *state, char *number)
+{
+    char firstname[10];
+    char lastname[10];
+    
+    printf("Please enter your First name: ");
+    scanf("%s",firstname);
+    
+    printf("Please enter your Last name: ");
+    scanf("%s",lastname);
+     
+    struct Owner nOwner;
+    strcpy(nOwner.firstName ,firstname);
+    strcpy(nOwner.lastName ,lastname);
+    strcpy(nOwner.licencePlateInfo.state, state);
+    strcpy(nOwner.licencePlateInfo.platenumber ,number);
+    
+    
+    return nOwner;
+}
+
+char * getCurrentDirectory()
+{
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+        fprintf(stdout, "Current working dir: %s\n", cwd);
+    else
+        perror("getcwd() error");
+    strcat(cwd, "/test.xml");
+    
+    return cwd;
+}
+
+void WriteToXML(Owner owner)
+{
+    FILE *fp ;
+    char * cwd = getCurrentDirectory();
+    fp = fopen(cwd, "a");
+    fprintf(fp,"<owner firstName='%s' lastName='%s'> \n  <state>%s</state>  \n  <code>%s</code>  \n</owner> \n",owner.firstName,owner.lastName,owner.licencePlateInfo.state,owner.licencePlateInfo.platenumber);
+    fclose(fp);
+}
+
+
+
+
+  //  Owner owner;
+    
+  //  owner = newOwner();
+    
+  //  WriteToXML(owner);
+    
+
+
 

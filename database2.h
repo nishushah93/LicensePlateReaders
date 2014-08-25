9e91816a53af5a2a3ee2cdfb3c1c02aac54e9735
sj33 <sj33@drexel.edu>
2014-08-25 14:31:43 -0400

    Create database2.h

---
+++ database2.h
@@ -0,0 +1,26 @@
+
+#ifndef database_h_
+#define database_h_
+
+struct LicencePlate
+{
+    char state[3];
+    char platenumber[7];
+};
+struct Owner
+{
+    char lastName [10];
+    char firstName [10];
+    struct LicencePlate licencePlateInfo;
+};
+
+typedef struct Owner Owner;
+typedef struct LicencePlate LicencePlate;
+
+
+Owner newOwner(char *state, char *number);
+char * getCurrentDirectory();
+void WriteToXML(Owner owner);
+
+#endif /* FOO_H_ */
+
